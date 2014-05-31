3d7530ab40f7976e1a62734760e915f8cb910b5f
Mate Lakat <mlakat@mlakat.hu>
2014-05-31 20:41:10 +0100

    Added missing imports

--- project/java/test/eu/lakat/sonarexample/MainTest.java
+++ project/java/test/eu/lakat/sonarexample/MainTest.java
@@ -3,6 +3,8 @@ package eu.lakat.sonarexample;
 
 import org.junit.runners.JUnit4;
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 
 @RunWith(JUnit4.class)
