ac6bcb882912b185b47cfee685859a10904094e3
Mate Lakat <mlakat@mlakat.hu>
2014-05-31 20:27:48 +0100

    Added a stupid testclass

---
+++ project/java/test/eu/lakat/sonarexample/MainTest.java
@@ -0,0 +1,14 @@
+package eu.lakat.sonarexample;
+
+
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(JUnit4.class)
+public class MainTest {
+    @Test
+    public void someStupidTest() {
+        assertEquals(1, 1);
+    }
+}
