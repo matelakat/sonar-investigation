c599a1d3e60d0061f694cce56ac3262e21615040
Mate Lakat <mlakat@mlakat.hu>
2014-06-01 10:28:30 +0100

    Add duplication

---
+++ project/java/src/eu/lakat/sonarexample/DuplicatedLogic.java
@@ -0,0 +1,18 @@
+package eu.lakat.sonarexample;
+
+/**
+ * Created by matelakat on 01/06/14.
+ */
+public class DuplicatedLogic {
+    public boolean duplicateComplicatedImplementation(String otherParamRenamed) {
+        if('a' == otherParamRenamed.charAt(0)) {
+            if('b' == otherParamRenamed.charAt(1)) {
+                return true;
+            }
+            if('c' == otherParamRenamed.charAt(1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
