ace3d04dba62dd952da892385bab4644597824a0
Mate Lakat <mlakat@mlakat.hu>
2014-06-01 10:54:47 +0100

    Add duplication

--- project/java/src/eu/lakat/sonarexample/ExactDuplicate.java
+++ project/java/src/eu/lakat/sonarexample/ExactDuplicate.java
@@ -15,4 +15,16 @@ public class ExactDuplicate {
         }
         return false;
     }
+
+    public boolean ratherComplicatedImplementation2(String param) {
+        if('a' == param.charAt(0)) {
+            if('b' == param.charAt(1)) {
+                return true;
+            }
+            if('c' == param.charAt(1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
