950c02e6de59d59472eccb96ae9349fc62a1803d
Mate Lakat <mlakat@mlakat.hu>
2014-06-01 10:04:00 +0100

    Create separate integrationtest

---
+++ project/java/test/eu/lakat/sonarexample/IntegrationTest.java
@@ -0,0 +1,11 @@
+package eu.lakat.sonarexample;
+
+/**
+ * Created by matelakat on 01/06/14.
+ */
+public class IntegrationTest {
+    public static void main(String[] args) {
+        Utility utility = new Utility();
+        utility.sayHello();
+    }
+}
