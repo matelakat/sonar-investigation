0b5fefb0cb56a0ddccb277ff52ddeec2b66fd188
Mate Lakat <mlakat@mlakat.hu>
2014-06-02 23:41:30 +0100

    Do not change directory

--- project/python/runtests.sh
+++ project/python/runtests.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -eux
 
-(
-cd src
 rm -f .coverage coverage.xml nosetests.xml
-)
 
 export SONAR_RUNNER_HOME=$(readlink -f ../../../sonar-runner/sonar-runner-2.4/)
 export PATH=$PATH:$SONAR_RUNNER_HOME/bin:$(readlink -f ../../../sonar/jdk1.7.0_60/bin/)
@@ -26,15 +23,12 @@ set +u
 . .env/bin/activate
 set -u
 
-(
-cd src
 coverage erase
 set +e
-coverage run --branch --source=mymodule $(which nosetests) --with-xunit mymodule
+coverage run --branch --source=src $(which nosetests) --with-xunit
 TESTRESULT=$?
 set -e
 coverage xml -i
-)
 
 # Publish stuff with sonar
 sonar-runner -X
