7e0d42211602e03f71bceba28f053f6cd2dc88f0
Mate Lakat <mlakat@mlakat.hu>
2014-06-02 22:36:41 +0100

    Clean files first

--- project/python/runtests.sh
+++ project/python/runtests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
+rm -f .coverage coverage.xml nosetests.xml
+
 export SONAR_RUNNER_HOME=$(readlink -f ../../../sonar-runner/sonar-runner-2.4/)
 export PATH=$PATH:$SONAR_RUNNER_HOME/bin:$(readlink -f ../../../sonar/jdk1.7.0_60/bin/)
 
