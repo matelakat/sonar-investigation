75fb11f4a02c2044c99643e71d144fecf73d456f
Mate Lakat <mlakat@mlakat.hu>
2014-06-02 23:03:53 +0100

    Move things to src folder

--- project/python/runtests.sh
+++ project/python/runtests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -eux
 
+(
+cd src
 rm -f .coverage coverage.xml nosetests.xml
+)
 
 export SONAR_RUNNER_HOME=$(readlink -f ../../../sonar-runner/sonar-runner-2.4/)
 export PATH=$PATH:$SONAR_RUNNER_HOME/bin:$(readlink -f ../../../sonar/jdk1.7.0_60/bin/)
@@ -23,12 +26,15 @@ set +u
 . .env/bin/activate
 set -u
 
+(
+cd src
 coverage erase
 set +e
 coverage run --branch --source=mymodule $(which nosetests) --with-xunit mymodule
 TESTRESULT=$?
 set -e
 coverage xml -i
+)
 
 # Publish stuff with sonar
 sonar-runner -X
